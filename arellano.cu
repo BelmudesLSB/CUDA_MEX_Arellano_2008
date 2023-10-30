28e8b64bdd5ea9706b6ce1a85fbcb20e7162d9a3
Lucas Belmudes <belmu002@umn.edu>
2023-10-30 17:50:34 -0500

    To do: finish v_r

--- arellano.cu
+++ arellano.cu
@@ -68,13 +68,41 @@ __global__ void update_vd(double* d_V_d_0, double* d_V_d_1, double* d_V, double*
     d_V_d_1[y_id] = (1/(1-d_gamma)) * pow(d_y_grid_under_default[y_id], 1-d_gamma) + d_beta * (d_theta * E_v + (1-d_theta) * E_vd);
 }
 
+__global__ void update_vr_and_bond_policy(double* d_b_grid, double* d_Q_1, double* d_y_grid, double* d_p_grid, double* d_V_r_1, double* d_V, int* d_bond_policy){
+    int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+    double aux_v = -100000000; 
+    if (thread_id < d_b_grid_size * d_y_grid_size)
+    {
+        for (int x = 0; x<d_b_grid_size; x++)
+        {
+            double E_V_rx = 0;
+            double c = d_y_grid[blockIdx.x] - d_Q_1[blockIdx.x * d_b_grid_size + x] * d_b_grid[x] + d_b_grid[threadIdx.x];
+            if (c >= 0)
+            {
+                for (int y_prime = 0; y_prime< d_y_grid_size; y_prime++)
+                {
+                    E_V_rx += d_p_grid[blockIdx.x * d_y_grid_size + y_prime] * d_V[y_prime * d_b_grid_size + x];
+                }
+            }
+            double temp = (1/(1-d_gamma)) * pow(c, 1-d_gamma) + d_beta * E_V_rx;
+            if (temp >= aux_v)
+            {
+                aux_v = temp;
+                d_bond_policy[thread_id] = x;
+            }
+            d_V_r_1[thread_id] = aux_v;
+        }
+    }
+    
+}
+
 void solve_arellano_model(Parameters_host parms, double* d_b_grid, double* d_y_grid, double* d_p_grid, double* d_y_grid_under_default, double* d_V, double* d_V_d_0, double* d_V_d_1, double* d_V_r_0, double* d_V_r_1, double* d_Q_0, double* d_Q_1, int* d_default_policy, int* d_bond_policy){
     mexPrintf("Running GPU code...\n");
     d_guess_vd_vr_q<<<parms.y_grid_size, parms.b_grid_size>>>(d_V_r_0, d_V_d_0, d_Q_0);
     update_v_and_default_policy<<<parms.y_grid_size, parms.b_grid_size>>>(d_V, d_V_d_0, d_V_r_0, d_default_policy);
     update_price<<<parms.y_grid_size, parms.b_grid_size>>>(d_default_policy, d_Q_1, d_p_grid);
     update_vd<<<1, parms.y_grid_size>>>(d_V_d_0, d_V_d_1, d_V, d_p_grid, d_y_grid_under_default);
-    //update_vr_and_bond_policy<<<parms.y_grid_size, parms.b_grid_size>>>(d_b_grid, d_Q_1, d_y_grid, d_p_grid, d_V_r_1, d_V, d_bond_policy);
+    update_vr_and_bond_policy<<<parms.y_grid_size, parms.b_grid_size>>>(d_b_grid, d_Q_1, d_y_grid, d_p_grid, d_V_r_1, d_V, d_bond_policy);
 }
 
 void fill_device_constants(Parameters_host parms){
--- arellano.h
+++ arellano.h
@@ -11,6 +11,8 @@ __global__ void update_price(int d_default_policy, double* d_Q_1, double* d_p_gr
 
 __global__ void update_vd(double* d_V_d_0, double* d_V_d_1, double* d_V, double* d_p_grid, double* d_y_grid_under_default);
 
+__global__ void update_vr_and_bond_policy(double* d_b_grid, double* d_Q_1, double* d_y_grid, double* d_p_grid, double* d_V_r_1, double* d_V, int* d_bond_policy);
+
 void solve_arellano_model(Parameters_host parms, double* d_b_grid, double* d_y_grid, double* d_p_grid, double* d_y_grid_under_default, double* d_V, double* d_V_d_0, double* d_V_d_1, double* d_V_r_0, double* d_V_r_1, double* d_Q_0, double* d_Q_1, int* d_default_policy, int* d_bond_policy);
 
 void fill_device_constants(Parameters_host parms);
